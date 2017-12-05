cc4a8a0cf438ca25c39b88dd1b55e940ace7ca77
Ricardo Coelho <coelhora@gmail.com>
2017-12-05 19:30:28 -0200

    Codigo comentado removido

--- ABP/debug.h
+++ ABP/debug.h
@@ -1,11 +1,4 @@
 #include <stdio.h>
-//#ifdef DEBUG
-//#define dprintf(fmt, ...) \
-//    do{ fprintf(stdout, fmt, __VA_ARGS__ ); } while(0)
-//#else
-//#define dprintf(...)
-//#endif
-
 
 #ifdef DEBUG
 #define dprintf(fmt, ...) \
