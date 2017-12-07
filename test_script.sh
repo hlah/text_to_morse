9c9848b67023aef46b906a0ae8d9fd7437c2c0e4
ricardocoelho <coelhora@gmail.com>
2017-12-07 15:54:36 -0200

    troca arquivo de saida da conversao por /dev/null.

--- test_script.sh
+++ test_script.sh
@@ -2,6 +2,6 @@
 touch log.txt
 for i in `seq 1 $1`;
 do
-    ./morse.run Exemplo/TabelaMorse.txt Exemplo/TheGodfather-MarioPuzo-Chapter1-English.txt b.out >> log.txt
-    ./morse.run Exemplo/TabelaMorse.txt Exemplo/TheGodfather-MarioPuzo-Chapter1-English.txt b.out avl >> log.txt
+    ./morse.run Exemplo/TabelaMorse.txt Exemplo/TheGodfather-MarioPuzo-Chapter1-English.txt /dev/null >> log.txt
+    ./morse.run Exemplo/TabelaMorse.txt Exemplo/TheGodfather-MarioPuzo-Chapter1-English.txt /dev/null avl >> log.txt
 done
