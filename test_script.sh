14a2b3d33a1810d21327cdcf718ebf4ba10e80ed
Mateus Davi Simon <hlah_@hotmail.com>
2017-12-09 11:42:31 -0200

    Adicionado combinações com tabela ordenada/desordenada e arquivos grande e pequeno script de teste.

--- test_script.sh
+++ test_script.sh
@@ -7,6 +7,16 @@ rm -f log.txt
 
 for i in `seq 1 $1`;
 do
+	echo -e '\nConvertendo Arquivo pequeno com Tabela ordenada:' >> log.txt
     build/morse.run Exemplo/TabelaMorse.txt Exemplo/TheGodfather-MarioPuzo-Chapter1-English.txt /dev/null >> log.txt
     build/morse.run Exemplo/TabelaMorse.txt Exemplo/TheGodfather-MarioPuzo-Chapter1-English.txt /dev/null avl >> log.txt
+	echo -e '\nConvertendo Arquivo pequeno com Tabela desordenada:' >> log.txt
+    build/morse.run Exemplo/TabelaMorseAlt.txt Exemplo/TheGodfather-MarioPuzo-Chapter1-English.txt /dev/null >> log.txt
+    build/morse.run Exemplo/TabelaMorseAlt.txt Exemplo/TheGodfather-MarioPuzo-Chapter1-English.txt /dev/null avl >> log.txt
+	echo -e '\nConvertendo Arquivo grande com Tabela ordenada:' >> log.txt
+    build/morse.run Exemplo/TabelaMorse.txt Exemplo/LesMiserablesPlus.txt /dev/null >> log.txt
+    build/morse.run Exemplo/TabelaMorse.txt Exemplo/LesMiserablesPlus.txt /dev/null avl >> log.txt
+	echo -e '\nConvertendo Arquivo grande com Tabela desordenada:' >> log.txt
+    build/morse.run Exemplo/TabelaMorseAlt.txt Exemplo/LesMiserablesPlus.txt /dev/null >> log.txt
+    build/morse.run Exemplo/TabelaMorseAlt.txt Exemplo/LesMiserablesPlus.txt /dev/null avl >> log.txt
 done
